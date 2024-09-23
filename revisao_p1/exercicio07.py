7a3e55485ac568e1a2dccab12456da30770a5f81
bruno5477 <brunooliveira.tec@gmail.com>
2024-09-22 22:46:31 -0300

    novos

---
+++ revisao_p1/exercicio07.py
@@ -0,0 +1,32 @@
+dicionario_alunos = {}
+def notas_aluno(info:list):
+    
+
+    for nome, nota in info:
+        dicionario_alunos[nome] = nota
+    return dicionario_alunos
+
+def consultar(dicionario:dict, nome:str):
+    if nome in dicionario:
+        print(f"{nome} tema a nota igual à : {dicionario[nome]}")
+    else: 
+        print(f"O Aluno {nome} não foi encontrado.")
+
+    
+
+def atualizar_nota(dicionario: dict, nome: str, nota:float):
+    if nome in dicionario:
+        dicionario[nome] = nota
+    else:
+        print(f"O aluno {nome} não foi encontrado.")
+
+
+alunos = [("Bruno", 9.5), ("Kuze", 10), ("Alya", 10)]
+
+print(notas_aluno(alunos))
+
+consultar(dicionario_alunos, "Alya")
+consultar(dicionario_alunos, "Rosonatt")
+
+atualizar_nota(dicionario_alunos,"Bruno", 10)
+print(dicionario_alunos)
---
+++ revisao_p1/exerciocio08.py
@@ -0,0 +1,24 @@
+estoque_loja = {
+    "Notebook": 120,
+    "Smartphone": 5,
+    "Fones de Ouvido": 20,
+    "Teclado": 15,
+    "Monitor": 8,
+    "Processador": 10,
+    "Gabinete":10,
+    "Mouse": 13
+}
+
+def verificar_estoque(produto:str):
+    if produto in estoque_loja:
+        unidades = estoque_loja[produto]
+    
+        if unidades > 0 :
+            print(f"O produto '{produto}' está disponível e temos {unidades} unidades no estoque. ")
+        else:    
+            print(f"O produto '{produto}' não está disponível no estoque. ")
+    else:
+        print(f"O produto '{produto}' não foi encontrado no estoque.")
+
+verificar_estoque("Teclado")
+verificar_estoque("Cooler")
