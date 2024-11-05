7fb94b9f8e4776895794664558820dac6f1f987a
bruno5477 <brunooliveira.tec@gmail.com>
2024-11-05 19:41:57 -0300

    vai

--- Trabalho/bucket_sort.py
+++ Trabalho/bucket_sort.py
@@ -40,4 +40,4 @@ def bucket_sort(lista):
 # Exemplo de uso
 lista = [0.78, 0.17, 0.39, 0.26, 0.72, 0.94, 0.21, 0.12, 0.23, 0.68]
 lista_ordenada = bucket_sort(lista)
-print(f"\nLista não ordenada: {lista} \nLista ordenado: {lista_ordenada}\n")a
+print(f"\nLista não ordenada: {lista} \nLista ordenado: {lista_ordenada}\n")
